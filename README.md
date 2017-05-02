5e3dd3da1d550757147517476097230c09f8cf21
Kevin <aoxianglele2010@gmail.com>
2017-05-02 23:52:57 +0800

    Initial commit.

--- README.md
+++ README.md
@@ -11,29 +11,34 @@ Dolores后端存储使用LDAP协议，本文档会帮助您顺利的搭建一台
 Openldap安装完成以后会在 `/usr/local/etc/openldap`(根据编译时`./configure --prefix`参数可能会略有差异)生成默认的配置文件。
 
  1. 打开`slapd.ldif`文件
- 2. 给`config`添加`olcRootDN`和 `olcRootPW`
- 3. 给`mdb`数据库添加`olcRootDN`和 `olcRootPW`
+ 2. 给`mdb`数据库添加`olcRootDN`和 `olcRootPW`
+ 3. 下载`dolores`[自定义`schema`文件](https://www.github.com/DoloresTeam/dolores-ldap-init/schema/dolores.ldif) 然后在`slapd.ldif`中 `incloud path/to/core.ldif`的下方添加 `incluce path/to/dolores.ldif`
  4. 将`slapd.ldif`导入`config`数据库，命令类似 `./slapdadd -l /usr/local/etc/openldap/slapd.ldif -F /usr/local/etc/openldap -n 0 -d 3`
- 
-#### 0x01.初始化
-##### 获取`Dolores`初始化程序
 
+#### 0x01.初始化
+
+##### 启动`LDAP`服务
+ `sudo ./slapd -F /usr/local/etc/openldap -d 3`
+
+##### 获取`Dolores`初始化程序
 ```
 go get https://www.github.com/DoloresTeam/dolores-ldap-init
 ```
 ##### 配置`Dolores`初始化程序
 ``` yarm
-
+host: 127.0.0.1 # ldap host
+port: 389
+subfix: dc=dolores,dc=org
+rootdn: cn=admin,dc=dolores,dc=org # 注意rootDN的后缀要和上面一致
+rootpwd: secret
+companyname: Dolores科技有限公司
 ```
-##### 启动`LDAP`服务
- `sudo ./slapd -F /usr/local/etc/openldap -d 3`
-
 ##### 编译执行初始化程序
 ``` bash
 cd dolores-ldap-init
 go build
-./dolores-ldap-init -f ./config.yarm
+./dolores-ldap-init -path ./conf.yaml
 ```
 
 ####0x02. 测试
-使用ldap可视化客户端[Apache Directory Studio](http://directory.apache.org/studio/)链接Ldap服务器，展开所有`DIT`，和下图结构类似，说明初始化成功。（应该紧有名字不一致）
+使用ldap可视化客户端[Apache Directory Studio](http://directory.apache.org/studio/)链接Ldap服务器，展开所有`DIT`，和下图结构类似，说明初始化成功。（应该紧有名字不一致）![org](./asset/org.png)
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	ldap "gopkg.in/ldap.v2"
+	yaml "gopkg.in/yaml.v1"
+)
+
+type Config struct {
+	Host        string
+	Port        int
+	RootDN      string
+	RootPWD     string
+	Subfix      string
+	CompanyName string
+}
+
+var config = Config{}
+
+func main() {
+
+	configFilePath := flag.String(`path`, `./conf.yaml`, `配置文件路径`)
+
+	flag.Parse()
+
+	_, err := os.Stat(*configFilePath)
+	if !(err == nil || os.IsExist(err)) {
+		panic(`配置文件不存在`)
+	}
+	data, _ := ioutil.ReadFile(*configFilePath)
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Panic(`配置文件不正确`)
+	}
+
+	log.Printf(`config: [%s]`, config)
+
+	log.Print(`will dial ldap server`)
+	// 链接ldap服务器
+	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", config.Host, config.Port))
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	log.Print(`did dial ldap server`)
+	log.Print(`will bind database`)
+	// 绑定config数据库
+	err = l.Bind(config.RootDN, config.RootPWD)
+	if err != nil {
+		panic(err)
+	}
+	log.Print(`did bind database`)
+
+	log.Print(`1. create root entry`)
+
+	aq := ldap.NewAddRequest(config.Subfix)
+	aq.Attribute(`objectClass`, []string{`organization`, `dcObject`, `top`})
+	aq.Attribute(`o`, []string{`root created by dolores ldap init tool`})
+	checkError(l.Add(aq))
+
+	log.Print(`2. create unit`)
+	aq = ldap.NewAddRequest(`ou=unit,` + config.Subfix)
+	aq.Attribute(`objectClass`, []string{`organizationalUnit`, `top`})
+	aq.Attribute(`ou`, []string{`unit`})
+	checkError(l.Add(aq))
+
+	log.Print(`2.1 create organization`)
+	aq = ldap.NewAddRequest(`oid=1,ou=unit,` + config.Subfix)
+	aq.Attribute(`objectClass`, []string{`organization`, `unitExtended`, `top`})
+	aq.Attribute(`o`, []string{config.CompanyName})
+	aq.Attribute(`oid`, []string{`1`})
+
+	checkError(l.Add(aq))
+
+	log.Print(`3. create person`)
+	aq = ldap.NewAddRequest(`ou=person,` + config.Subfix)
+	aq.Attribute(`objectClass`, []string{`organizationalUnit`, `top`})
+	aq.Attribute(`ou`, []string{`person`})
+	checkError(l.Add(aq))
+
+	log.Print(`4. create permission`)
+	aq = ldap.NewAddRequest(`ou=permission,` + config.Subfix)
+	aq.Attribute(`objectClass`, []string{`organizationalUnit`, `top`})
+	aq.Attribute(`ou`, []string{`permission`})
+	checkError(l.Add(aq))
+
+	log.Print(`4.1 create unit permission`)
+	aq = ldap.NewAddRequest(`ou=unit,ou=permission,` + config.Subfix)
+	aq.Attribute(`objectClass`, []string{`organizationalUnit`, `top`})
+	aq.Attribute(`ou`, []string{`unitPermission`})
+	checkError(l.Add(aq))
+
+	log.Print(`4.2 create person permission`)
+	aq = ldap.NewAddRequest(`ou=person,ou=permission,` + config.Subfix)
+	aq.Attribute(`objectClass`, []string{`organizationalUnit`, `top`})
+	aq.Attribute(`ou`, []string{`personPermission`})
+	checkError(l.Add(aq))
+
+	log.Print(`5. create role`)
+	aq = ldap.NewAddRequest(`ou=role,` + config.Subfix)
+	aq.Attribute(`objectClass`, []string{`organizationalUnit`, `top`})
+	aq.Attribute(`ou`, []string{`role`})
+	checkError(l.Add(aq))
+
+	log.Print(`initial done ~`)
+}
+
+func checkError(e error) {
+	if e != nil {
+		fmt.Println(e)
+	}
+}
